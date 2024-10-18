92f8d038c7a7f51ba062614e4bd6da65a82ff485
dragana_djurdjevic <dragana.djurdjevic@sperasoft.com>
2024-10-18 11:24:20 +0200

    modify sound system
    added music methods and modify sounds mnethods

--- Source/SoundSystem/SoundSystem.cpp
+++ Source/SoundSystem/SoundSystem.cpp
@@ -2,8 +2,9 @@
 #include "Common/Logs.hpp"
 #include <iostream>
 
+// SOUND
 // Load audio and save to folder
-bool SoundSystem::loadSound(const std::string& soundName, const std::string& filePath) 
+bool SoundSystem::addSound(int32_t soundID, const std::string& filePath)
 {
 	sf::SoundBuffer buffer;
 	if (!buffer.loadFromFile(filePath)) 
@@ -11,72 +12,127 @@ bool SoundSystem::loadSound(const std::string& soundName, const std::string& fil
 		LOG("Failed to load sound from %s", filePath.c_str());
 		return false;
 	}
-	soundBuffers.emplace(soundName, std::move(buffer));
-	LOG("Successfully loaded sound: %s", soundName.c_str());
+	soundBuffers.emplace(soundID, std::move(buffer));
+	LOG("Successfully loaded sound: %d", soundID);
 	return true;
 }
 
 // Playing sound from the buffer
-void SoundSystem::playSound(const std::string& soundName) 
+void SoundSystem::playSound(int32_t soundID)
 {
-	if (soundBuffers.find(soundName) != soundBuffers.end()) 
+	if (soundBuffers.find(soundID) != soundBuffers.end())
 	{
 		auto sound = std::make_unique<sf::Sound>();
-		sound->setBuffer(soundBuffers[soundName]);
+		sound->setBuffer(soundBuffers[soundID]);
 		sound->setVolume(100.f);
 		sound->play();
 
 		// Saving active sounds
-		activeSounds.push_back(std::move(sound));
-		LOG("Playing sound: %s", soundName.c_str());
+		activeSounds.push_back({ soundID, std::move(sound) });
+		LOG("Playing sound: %d", soundID);
 	}
 	else 
 	{
-		LOG("Sound %s not found!", soundName.c_str());
+		LOG("Sound %d not found!", soundID);
 	}
 }
 
 // Stop sound
-void SoundSystem::stopSound(const std::string& soundName)
+void SoundSystem::stopSound(int32_t soundID)
 {
-	for (auto& sound : activeSounds) 
+	for (auto it = activeSounds.begin(); it != activeSounds.end(); ++it)
 	{
-		if (sound->getBuffer() == &soundBuffers[soundName]) 
+		if (it->first == soundID)
 		{
-			sound->stop();
-			LOG("Stopped sound: %s", soundName.c_str());
+			it->second->stop();
+			LOG("Stopped sound: %d", soundID);
 		}
 	}
 }
 
 // Pause sound
-void SoundSystem::pauseSound(const std::string& soundName)
+void SoundSystem::pauseSound(int32_t soundID)
 {
-	for (auto& sound : activeSounds) 
+	for (auto& soundPair : activeSounds)
 	{
-		if (sound->getBuffer() == &soundBuffers[soundName]) 
+		if (soundPair.first == soundID)
 		{
-			sound->pause();
-			LOG("Paused sound: %s", soundName.c_str());
+			soundPair.second->pause();
+			LOG("Paused sound: %d", soundID);
 		}
 	}
 }
 
 // Check if sound is playing
-bool SoundSystem::isSoundPlaying(const std::string& soundName) const
+bool SoundSystem::isSoundPlaying(int32_t soundID) const
 {
-	auto it = soundBuffers.find(soundName);
-	if (it == soundBuffers.end()) 
+	for (const auto& soundPair : activeSounds)
 	{
-		return false;
-	}
-
-	for (const auto& sound : activeSounds) 
-	{
-		if (sound->getBuffer() == &it->second && sound->getStatus() == sf::Sound::Playing) 
+		if (soundPair.first == soundID && soundPair.second->getStatus() == sf::Sound::Playing)
 		{
 			return true;
 		}
 	}
 	return false;
+}
+
+// MUSIC
+// Load music from file and store it
+bool SoundSystem::addMusic(int32_t musicID, const std::string& filePath)
+{
+	auto music = std::make_unique<sf::Music>();
+	if (!music->openFromFile(filePath))
+	{
+		LOG("Failed to load music from %s", filePath.c_str());
+		return false;
+	}
+	musicTracks.emplace(musicID, std::move(music));
+	LOG("Successfully loaded music: %d", musicID);
+	return true;
+}
+
+// Play music
+void SoundSystem::playMusic(int32_t musicID)
+{
+	auto it = musicTracks.find(musicID);
+	if (it != musicTracks.end())
+	{
+		currentMusic = std::move(it->second);
+		currentMusic->setVolume(100.f);
+		currentMusic->play();
+		LOG("Playing music: %d", musicID);
+	}
+	else
+	{
+		LOG("Music %d not found!", musicID);
+	}
+}
+
+// Stop music
+void SoundSystem::stopMusic()
+{
+	if (currentMusic)
+	{
+		currentMusic->stop();
+		LOG("Stopped music");
+
+		// Clear current music after stopping
+		currentMusic.reset();
+	}
+}
+
+// Pause music
+void SoundSystem::pauseMusic()
+{
+	if (currentMusic)
+	{
+		currentMusic->pause();
+		LOG("Paused music");
+	}
+}
+
+// Check if music is playing
+bool SoundSystem::isMusicPlaying() const
+{
+	return currentMusic && currentMusic->getStatus() == sf::Music::Playing;
 }
--- Source/SoundSystem/SoundSystem.hpp
+++ Source/SoundSystem/SoundSystem.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <SFML/Audio.hpp>
-#include <string>
 #include <map>
 #include <list>
 #include <memory>
@@ -9,21 +8,42 @@
 class SoundSystem
 {
 public:
-	bool loadSound(const std::string& soundName, const std::string& filePath);
+	// SOUNDS: used for small sounds (gun shots, foot steps, etc.)
+	bool addSound(int32_t soundID, const std::string& filePath);
 
-	void playSound(const std::string& soundName);
+	void playSound(int32_t soundID);
 
-	void stopSound(const std::string& soundName);
+	void stopSound(int32_t soundID);
 
-	void pauseSound(const std::string& soundName);
+	void pauseSound(int32_t soundID);
 
-	bool isSoundPlaying(const std::string& soundName) const;
+	bool isSoundPlaying(int32_t soundID) const;
+
+	// MUSIC: used to play compressed music that lasts several minutes
+
+	bool addMusic(int32_t musicID, const std::string& filePath);
+
+	void playMusic(int32_t musicID);
+
+	void stopMusic();
+
+	void pauseMusic();
+
+	bool isMusicPlaying() const;
 
 private:
+	//SOUNDS
 	// Map that stores sound buffers
-	std::map<std::string, sf::SoundBuffer> soundBuffers;
+	std::map<int32_t, sf::SoundBuffer> soundBuffers;
 
-	// List of unique pointers to active sounds
-	std::list<std::unique_ptr<sf::Sound>> activeSounds;
+	// List of active sounds, paired with their soundID
+	std::list<std::pair<int32_t, std::unique_ptr<sf::Sound>>> activeSounds;
+
+	//MUSIC
+	// Map that stores music for background music
+	std::map<int32_t, std::unique_ptr<sf::Music>> musicTracks;
+
+	// Unique pointer for the currently playing music
+	std::unique_ptr<sf::Music> currentMusic;
+
 };
-
