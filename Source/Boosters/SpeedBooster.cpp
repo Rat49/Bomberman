46e8e632d57027ea4365a7d14d27dbd3f48976bd
Tijana Brkovic <tijana.brkovic@sperasoft.com>
2024-12-17 15:11:26 +0100

    SpeedBooster base class

---
+++ Source/Boosters/SpeedBooster.cpp
@@ -0,0 +1,15 @@
+#include "SpeedBooster.hpp"
+
+void SpeedBooster::applyEffect(PlayerCharacter& playerCharacter)
+{
+}
+
+bool SpeedBooster::removeEffect(PlayerCharacter& playerCharacter)
+{
+    return false;
+}
+
+bool SpeedBooster::shouldRemoveEffect()
+{
+    return false;
+}
