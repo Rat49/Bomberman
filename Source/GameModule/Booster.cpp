b9fcc18d54071d1ee1b6addf56fdca347cf312cf
SPERASOFT\lazar.jovicic <lazar.jovicic@sperasoft.com>
2024-12-30 16:11:10 +0100

    Bug fixes

--- Source/GameModule/Booster.cpp
+++ Source/GameModule/Booster.cpp
@@ -78,3 +78,8 @@ void Booster::setCallbackID(EventID boosterPickupID)
 {
 	m_boosterPickupID = boosterPickupID;
 }
+
+void Booster::emitCallback() const
+{
+    Modules::Events->emit(m_boosterPickupID, nullptr);
+}
